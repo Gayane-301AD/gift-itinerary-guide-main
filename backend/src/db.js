7424d7d72f9f3f2e9bfbe68ddc7ec41fe40da65d
Gayane-301AD <gayane.hovhannisyan@301AD.onmicrosoft.com>
2025-08-06 18:30:12 +0400

    Changes for Vercel Deployment

--- backend/src/db.js
+++ backend/src/db.js
@@ -12,12 +12,8 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'sasasa',
-  database: process.env.DB_NAME || 'whattocarry',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
 });
 
 export default pool; 
